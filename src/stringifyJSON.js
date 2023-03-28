545436aa23594bc9a9b27640839ea99be486df48
Rachel <rkmiller131@gmail.com>
2023-03-27 20:06:03 -0700

    passing tests for className, almost complete for stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,62 @@
 
 // but you don't so you're going to write it from scratch:
 
+// I - any type of object (string, bool, number, fn, etc)
+// O - returns a stringified version of the entire obj value
+// C - can't use native stringify
+// E - Nested obj structures
+
+// strategy
+// eventually build out a string, mimic the structure of JSONstringify
+// several subproblems:
+// what to do for the case of primitives
+// what to do for the case of objects
+// what to do for the case of arrays
+// what to do for the case of functions
+// recurse over specifically the objects and arrays because those can be nested
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+  // Edge case: invlaid input
+  if (typeof obj === 'function' || obj === undefined) {
+    result += '';
+  }
+  // Base Case: primitives
+  if (typeof obj === 'string') {
+    return result + '"' + obj + '"';
+  }
+  if (typeof obj === 'number' || typeof obj === 'null' || typeof obj === 'boolean') {
+    return result + obj;
+  }
+
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return result + '[]';
+    }
+    result += '[';
+    obj.forEach(function(value, index, array) {
+      if (index === array.length - 1) {
+        result += stringifyJSON(value) + ']';
+      } else {
+        result += stringifyJSON(value) + ',';
+      }
+    });
+  } else if (typeof obj === 'object') {
+    console.log(obj);
+    if (Object.keys(obj).length === 0) {
+      result += 'null';
+    } else {
+      result += '{';
+      for (var key in obj) {
+        result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+      }
+      result += '}';
+    }
+  }
+
+  return result;
+  // console.log(result);
 };
+
+console.log(stringifyJSON({}));
+
